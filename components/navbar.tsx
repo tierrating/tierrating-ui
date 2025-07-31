19009bf72f61d6d971a327d73afad53e0670b279
RatzzFatzz <github@ratzloeffel.de>
2025-07-31 14:27:07 +0200

    Disable search

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -85,22 +85,23 @@ export default function NavBar() {
                 </div>
 
                 <div className="flex flex-1 items-center justify-end gap-2">
-                    <div className="relative max-w-[240px] w-full max-lg:hidden">
-                        <Button
-                            variant="outline"
-                            onClick={() => window.location.href = "/search"}
-                            className="flex w-full items-center justify-between rounded-full border bg-background/50 px-3 py-1.5 text-sm text-muted-foreground shadow-none hover:bg-accent"
-                        >
-                            <div className="flex items-center gap-2">
-                                <Search className="h-4 w-4" />
-                                <span>Search</span>
-                            </div>
-                            <div className="flex items-center gap-1">
-                                <kbd className="rounded border bg-muted px-1.5 text-[10px] font-medium">⌘</kbd>
-                                <kbd className="rounded border bg-muted px-1.5 text-[10px] font-medium">K</kbd>
-                            </div>
-                        </Button>
-                    </div>
+                    {/*search bar - removed until search is implemented */}
+                    {/*<div className="relative max-w-[240px] w-full max-lg:hidden">*/}
+                    {/*    <Button*/}
+                    {/*        variant="outline"*/}
+                    {/*        onClick={() => window.location.href = "/search"}*/}
+                    {/*        className="flex w-full items-center justify-between rounded-full border bg-background/50 px-3 py-1.5 text-sm text-muted-foreground shadow-none hover:bg-accent"*/}
+                    {/*    >*/}
+                    {/*        <div className="flex items-center gap-2">*/}
+                    {/*            <Search className="h-4 w-4" />*/}
+                    {/*            <span>Search</span>*/}
+                    {/*        </div>*/}
+                    {/*        <div className="flex items-center gap-1">*/}
+                    {/*            <kbd className="rounded border bg-muted px-1.5 text-[10px] font-medium">⌘</kbd>*/}
+                    {/*            <kbd className="rounded border bg-muted px-1.5 text-[10px] font-medium">K</kbd>*/}
+                    {/*        </div>*/}
+                    {/*    </Button>*/}
+                    {/*</div>*/}
 
                     <DropdownMenu>
                         <DropdownMenuTrigger asChild>
