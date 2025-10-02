b78bcdee93bdc9ed544d882b8c488eb9ab40ba20
RatzzFatzz <github@ratzloeffel.de>
2025-10-03 00:21:56 +0200

    Fix suspense error with anilist auth

--- app/auth/anilist/page.tsx
+++ app/auth/anilist/page.tsx
@@ -3,13 +3,21 @@
 import {useRouter, useSearchParams} from "next/navigation";
 import {useAuth} from "@/contexts/AuthContext";
 import ProtectedRoute from "@/components/ProtectedRoute";
-import React, {useEffect} from "react";
+import React, {Suspense, useEffect} from "react";
 import {authorize} from "@/components/api/anilist-api";
 import LoadingPage from "@/components/loading-page";
 
-export const dynamic = 'force-dynamic'
-
 export default function AuthAniList() {
+    return (
+        <ProtectedRoute>
+            <Suspense>
+                <Auth></Auth>
+            </Suspense>
+        </ProtectedRoute>
+    );
+}
+
+function Auth() {
     const searchParams = useSearchParams()
     const router = useRouter()
     const {user, token} = useAuth()
@@ -18,11 +26,11 @@ export default function AuthAniList() {
         if (searchParams.has("code")) {
             const code = searchParams.get("code")
             authorize(user, token, code)
-            .then(data => {
-                if (data.message) {
-                    return Promise.reject(data.message)
-                }
-            }).catch(err => {
+                .then(data => {
+                    if (data.message) {
+                        return Promise.reject(data.message)
+                    }
+                }).catch(err => {
                 console.error(err);
             }).finally(() => router.push(`/user/${user}`))
         } else {
@@ -30,9 +38,5 @@ export default function AuthAniList() {
         }
     }, [user, token, router, searchParams])
 
-    return (
-        <ProtectedRoute>
-            <LoadingPage />
-        </ProtectedRoute>
-    );
+    return <LoadingPage />
 }
