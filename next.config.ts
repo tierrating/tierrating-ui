9984a8a8caf2f16314be061d834f43d61e699ff0
RatzzFatzz <github@ratzloeffel.de>
2025-11-10 17:13:36 +0100

    Add tmdb to allow remote urls

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        remotePatterns: [new URL("https://s4.anilist.co/file/anilistcdn/media/**/*.*")]
+        remotePatterns: [
+            new URL("https://s4.anilist.co/file/anilistcdn/media/**/*.*"),
+            new URL("https://image.tmdb.org/t/p/**/*.*")
+        ]
     },
     reactStrictMode: false,
     eslint: {
