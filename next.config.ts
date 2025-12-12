9d94acefe7c308525aa8cbbea61c6f1c0ddfcf9e
RatzzFatzz <github@ratzloeffel.de>
2025-12-12 13:03:04 +0100

    Fix nextjs 16 build

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,6 @@ const nextConfig: NextConfig = {
         ]
     },
     reactStrictMode: false,
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
     output: "standalone"
 };
 
